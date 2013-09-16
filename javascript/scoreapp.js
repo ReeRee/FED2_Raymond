d4becee2181b9047ec6a52a9ae1f9050a2a582a1
Raymond <raymondsanders@230.205.028.145.hva.nl>
2013-09-16 14:08:08 +0200

    Edited two files

---
+++ javascript/scoreapp.js
@@ -0,0 +1,32 @@
+//		[FED2 - Deeltoets 1 | Versie:0.1]
+Var SCOREAPP = SCOREAPP || {};
+
+ function(){
+
+ 	//		[Data Objecten]
+ 	SCOREAPP.schedule = {
+
+ 	}
+
+ 	SCOREAPP.ranking = {
+
+ 	}
+
+ 	SCOREAPP.game = {
+
+ 	}
+
+	//		[Controller]
+
+	//		[Router]
+	SCOREAPP.router = {
+	}
+
+	//		[Pages]
+	SCOREAPP.page = {
+	}
+
+	//		[DOM Ready]
+
+
+ })();
